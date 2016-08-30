a0290bd56cd2df186d2f90ac4351b1e126da1fb7
mfep <serfozo.lorinc@live.com>
2016-08-30 23:55:10 +0200

    added saw and playerhealth component

---
+++ Source/Code/CorePlugin/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Duality;
+
+namespace TilemapJam
+{
+	public class PlayerHealth : Component
+	{
+		public void HitBySaw ()
+		{
+			Log.Game.Write ("Player's been hit by a saw!");
+			PlayerDeath ();
+		}
+
+		void PlayerDeath ()
+		{
+			
+		}
+	}
+}
+
---
+++ Source/Code/CorePlugin/SawComponent.cs
@@ -0,0 +1,39 @@
+﻿using Duality;
+using Duality.Components;
+using Duality.Components.Physics;
+
+namespace TilemapJam
+{
+	[RequiredComponent (typeof (Transform))]
+	public class SawComponent : Component, ICmpUpdatable, ICmpCollisionListener
+	{
+		public float RotTime { get; set; }
+
+		public void OnUpdate ()
+		{
+			Rotate ();
+		}
+
+		void Rotate ()
+		{
+			float rot = MathF.TwoPi / RotTime / 60 * Time.TimeMult;
+			GameObj.Transform.TurnBy (rot);
+		}
+
+		public void OnCollisionBegin (Component sender, CollisionEventArgs args)
+		{
+			var playerHealth = args.CollideWith.GetComponent<PlayerHealth> ();
+			if (playerHealth != null) {
+				playerHealth.HitBySaw ();
+			}
+		}
+
+		public void OnCollisionEnd (Component sender, CollisionEventArgs args)
+		{			
+		}
+
+		public void OnCollisionSolve (Component sender, CollisionEventArgs args)
+		{
+		}
+	}
+}
