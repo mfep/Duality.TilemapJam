216a1b1e7e83b5184a225ab9cb76c8d6412eee75
mfep <serfozo.lorinc@live.com>
2016-08-31 18:02:20 +0200

    saw component movement

--- Source/Code/CorePlugin/SawComponent.cs
+++ Source/Code/CorePlugin/SawComponent.cs
@@ -8,16 +8,24 @@ namespace TilemapJam
 	public class SawComponent : Component, ICmpUpdatable, ICmpCollisionListener
 	{
 		public float RotTime { get; set; }
+		public float HorizontalSpeed { get; set; }
 
 		public void OnUpdate ()
 		{
 			Rotate ();
+			Move ();
 		}
 
 		void Rotate ()
 		{
 			float rot = MathF.TwoPi / RotTime / 60 * Time.TimeMult;
 			GameObj.Transform.TurnBy (rot);
+		}
+
+		void Move ()
+		{
+			var moveVect = new Vector3 (HorizontalSpeed / 60 * Time.TimeMult, 0, 0);
+			GameObj.Transform.MoveBy (moveVect);
 		}
 
 		public void OnCollisionBegin (Component sender, CollisionEventArgs args)
