da77d79f9f8b6d4edb6c6793b1ac1fae65e58c5a
mfep <serfozo.lorinc@live.com>
2016-08-31 19:22:15 +0200

    add pickup class

---
+++ Source/Code/CorePlugin/Pickup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Duality;
+using Duality.Components;
+
+namespace TilemapJam
+{
+	[RequiredComponent (typeof (Transform))]
+	public class Pickup : Component, ICmpUpdatable, ICmpInitializable
+	{
+		public bool IdleMove { get; set; }
+		public float IdleFreq { get; set; }
+		public float IdleAmpl { get; set; }
+
+		static Random random = new Random (Time.StartupTime.Millisecond);
+		Vector3 initialPos;
+		float phase;
+
+		public void OnUpdate ()
+		{
+			if (IdleMove) {
+				Idle ();
+			}
+		}
+
+		public void OnInit (InitContext context)
+		{
+			phase = random.NextFloat (MathF.TwoPi);
+			initialPos = GameObj.Transform.Pos;
+		}
+
+		public void OnShutdown (ShutdownContext context)
+		{
+
+		}
+
+		void Idle ()
+		{
+			float time = (float)Time.GameTimer.TotalSeconds;
+			var posOffset = Vector3.UnitY * IdleAmpl * MathF.Sin (time * IdleFreq * MathF.TwoPi + phase);
+			GameObj.Transform.MoveToAbs (initialPos + posOffset);
+		}
+	}
+}
+
